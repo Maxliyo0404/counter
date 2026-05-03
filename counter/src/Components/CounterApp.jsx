ead650b25aec6747ec6bafaa63850cfec3b5cc2a
Mahliyo <gasandigital13@gmail.com>
2026-05-03 19:20:25 +0500

    useState

--- counter/src/Components/CounterApp.jsx
+++ counter/src/Components/CounterApp.jsx
@@ -1,5 +1,7 @@
+import { useState } from "react";
 import "./CounterApp.css";
 function CounterApp (){
+    const {count, setCount} = useState(0)
     return (
         <>
           <div className="container">
