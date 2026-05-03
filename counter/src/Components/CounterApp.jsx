6c71cca332a72dd9adfd800e1f531c6269329f71
Mahliyo <gasandigital13@gmail.com>
2026-05-03 20:06:35 +0500

    incrament css

--- counter/src/Components/CounterApp.jsx
+++ counter/src/Components/CounterApp.jsx
@@ -5,7 +5,8 @@ function CounterApp (){
     const incCount = ()=>setCount(count + 1);
     return (
         <>
-          <h1>{count}</h1>
+        <div className="container"></div>
+          <h1 className="title">{count}</h1>
           <button className="inc" onClick={incCount}>Incrament</button>
           <button className="rest" onClick={() => setCount(0)}>Reset</button>
           <button className="dec" onClick={() => setCount(count - 1)}>Decrament</button>
