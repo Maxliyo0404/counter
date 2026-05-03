4bdc53ce63eef6e0347d670c62ba8a71a1b7c0da
Mahliyo <gasandigital13@gmail.com>
2026-05-03 19:31:08 +0500

    incrament

--- counter/src/Components/CounterApp.jsx
+++ counter/src/Components/CounterApp.jsx
@@ -7,7 +7,7 @@ function CounterApp (){
           <h1>{count}</h1>
           <button onClick={() => setCount(count + 1)}>Incrament</button>
           <button onClick={() => setCount(0)}>Reset</button>
-          <button onClick={() => setCount(count - 1)}>Dec</button>
+          <button onClick={() => setCount(count - 1)}>Decrament</button>
         </>
     )
 }
