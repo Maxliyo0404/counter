dc7dd3da0d2e2eeed29742da18752e7f3a2cd93f
Mahliyo <gasandigital13@gmail.com>
2026-05-05 22:36:06 +0500

    Cards react

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,9 +1,12 @@
 import CounterApp from "./Components/CounterApp";
+import CardsApp from "./Components/Cards/CardsApp";
 import "./App.css";
 function App (){
     return (
         <>
          <CounterApp/>
+         <CounterApp/>
+
         </>
     )
 }
